74a096f101ac6ab9e47cf309bdfb6fa3bd63879a
zoraaga1 <fati.sousse12@gmail.com>
2024-05-27 20:36:15 +0100

    task1

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,18 @@
+/* eslint-disable vars-on-top */
+/* eslint-disable no-var */
+export default function taskBlock(trueOrFalse) {
+  // eslint-disable-next-line block-scoped-var
+  var task = false;
+  // eslint-disable-next-line block-scoped-var
+  var task2 = true;
+
+  if (trueOrFalse) {
+    // eslint-disable-next-line no-redeclare
+    var task = true;
+    // eslint-disable-next-line no-redeclare
+    var task2 = false;
+  }
+
+  // eslint-disable-next-line block-scoped-var
+  return [task, task2];
+}
