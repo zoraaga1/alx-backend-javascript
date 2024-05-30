be09c09d03675c426f1f85a256c5f108ddd17d0e
zoraaga1 <fati.sousse12@gmail.com>
2024-05-30 02:02:38 +0100

    tasks 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,18 +1,12 @@
-/* eslint-disable indent */
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
+// eslint-disable-next-line import/extensions
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-    return Promise
-        .all([uploadPhoto(), createUser()])
-        .then(([photoResponse, userResponse]) => {
-            console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
-        })
-        .catch((error) => {
-            console.error('Signup system offline');
-        });
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
-
-export default handleProfileSignup;
