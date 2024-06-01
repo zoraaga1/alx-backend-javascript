c9f7bbcded5b4385b8a4030d0cef52d09c6c2568
zoraaga1 <fati.sousse12@gmail.com>
2024-06-01 01:47:41 +0100

    all tasks

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable indent */
+export default class ClassRoom {
+    constructor(maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize;
+    }
+}
