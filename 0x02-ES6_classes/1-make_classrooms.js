e4394786f9de1187c060766cbfe0274207550a70
zoraaga1 <fati.sousse12@gmail.com>
2024-06-01 01:42:41 +0100

    all tasks

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,5 @@
-/* eslint-disable indent */
 import ClassRoom from './0-classroom';
 
-export default function intializeRooms() {
-    return [19, 20, 34].map((room) => new ClassRoom(room));
+export default function initializeRooms() {
+  return [19, 20, 34].map((room) => new ClassRoom(room));
 }
