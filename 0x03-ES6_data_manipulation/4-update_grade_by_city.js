935c9931a175f49efa3d6115bd15a1b32047a757
zoraaga1 <fati.sousse12@gmail.com>
2024-06-04 23:04:26 +0100

    tasks

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+	return students
+			.filter(student => student.city === city)
+			.map(student => {
+					const newGrade = newGrades.find(grade => grade.studentId === student.id);
+					return {
+							...student,
+							grade: newGrade ? newGrade.grade : 'N/A'
+					};
+			});
+}
+
+module.exports = updateStudentGradeByCity
