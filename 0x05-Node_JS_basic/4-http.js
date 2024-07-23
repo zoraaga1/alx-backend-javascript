300800d4e688e862a4147f7b460705c204a6497d
zoraaga1 <fati.sousse12@gmail.com>
2024-07-23 23:53:25 +0100

    task4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,17 +1,9 @@
-const http = require('http'); // Import the http module
+const http = require('http');
 
-// Create the HTTP server
 const app = http.createServer((req, res) => {
-  // Set the response header
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  // Write the response body
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
-// Make the server listen on port 1245
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
-});
+app.listen(1245);
 
-// Export the app
 module.exports = app;
