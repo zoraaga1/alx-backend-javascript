9fa323bcfc5e8255d2070ab9e6f0cb6f57153146
zoraaga1 <fati.sousse12@gmail.com>
2024-07-27 02:11:59 +0100

    task 3

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,26 @@
+class Utils {
+  function calculateNumber(type, a, b) {
+    const a_rounded = Math.round(a);
+    const b_rounded = Math.round(b);
+    let result = 0;
+    switch (type) {
+    case 'SUM':
+    result = a_rounded + b_rounded;
+    break;
+      case 'SUBTRACT':
+    result = a_rounded - b_rounded;
+    break;
+    case 'DIVIDE':
+      if (b_rounded === 0) {
+            result = "Error";
+      } else {
+            result = a_rounded / b_rounded;
+      }
+      break;
+    }
+    return result;
+  }
+  
+}
+
+module.exports = Utils;
