10798f8ff4d79929d5ef475657d7c7141362d50f
zoraaga1 <fati.sousse12@gmail.com>
2024-07-27 02:36:01 +0100

    task 3

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,26 +1,26 @@
 class Utils {
-  function calculateNumber(type, a, b) {
+  static calculateNumber(type, a, b) { // Remove the `function` keyword
     const a_rounded = Math.round(a);
     const b_rounded = Math.round(b);
     let result = 0;
     switch (type) {
     case 'SUM':
-    result = a_rounded + b_rounded;
-    break;
-      case 'SUBTRACT':
-    result = a_rounded - b_rounded;
-    break;
+        result = a_rounded + b_rounded;
+        break;
+    case 'SUBTRACT':
+        result = a_rounded - b_rounded;
+        break;
     case 'DIVIDE':
-      if (b_rounded === 0) {
-            result = "Error";
-      } else {
-            result = a_rounded / b_rounded;
-      }
-      break;
+        if (b_rounded === 0) {
+          result = "Error";
+        } else {
+          result = a_rounded / b_rounded;
+        }
+        break;
+
     }
     return result;
   }
-  
 }
 
-module.exports = Utils;
+module.exports = Utils; // Export the class
